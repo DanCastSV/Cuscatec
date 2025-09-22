ebc1f26536980281cbbdf463af9a5d7375e65ddd
DanCastSV <ec1335784@gmail.com>
2025-09-22 13:53:53 -0600

    creacion de los templates y el index.html

--- Cuscatec/cusca/views.py
+++ Cuscatec/cusca/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
 
 
-def index(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hola mundo!")
+from django.shortcuts import render
+
+def index(request):
+    return render(request, "cusca/index.html")  # ruta dentro de templates/cusca
 
 
 # Create your views here.
