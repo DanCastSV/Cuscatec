148c673990afca742e1bbb3a5fbe8362b4466767
DanCastSV <ec1335784@gmail.com>
2025-09-29 13:12:00 -0600

    Creacion de los static

--- Cuscatec/Cuscatec/settings.py
+++ Cuscatec/Cuscatec/settings.py
@@ -118,6 +118,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",   # carpeta "static" en tu proyecto
+]
+
+# Carpeta donde se recopilarán todos los archivos al hacer collectstatic
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
